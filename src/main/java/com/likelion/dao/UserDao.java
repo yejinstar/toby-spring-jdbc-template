e58da7d8c39a1c6f106c79b8a5177c04759cb0ba
yejinstar <gyj5586@gmail.com>
2022-10-25 14:58:00 +0900

    jdbcContextWithStatementStrategy 분리

--- src/main/java/com/likelion/dao/UserDao.java
+++ src/main/java/com/likelion/dao/UserDao.java
@@ -10,34 +10,54 @@ public class UserDao {
 
     private ConnectionMaker connectionMaker;
 
+    public UserDao() {
+        this.connectionMaker = new LocalConnectionMaker();
+    }
     public UserDao(ConnectionMaker connectionMaker) {
         this.connectionMaker = connectionMaker;
     }
 
-    public UserDao() {
-        this.connectionMaker = new LocalConnectionMaker();
+    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException, ClassNotFoundException {
+        Connection c = connectionMaker.makeConnection();
+        PreparedStatement pstmt = stmt.makePreparedStatement(c);
+        pstmt.executeUpdate();
+        pstmt.close();
+        c.close();
     }
 
     public void add (User user) throws ClassNotFoundException, SQLException{
 
-        Connection c = null;
-        PreparedStatement ps = null;
+        jdbcContextWithStatementStrategy(new StatementStrategy() {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement pstmt = null;
+                pstmt = connection.prepareStatement(
+                        "insert into users(id, name, password) values (?,?,?)");
+                pstmt.setString(1, user.getId());
+                pstmt.setString(2, user.getName());
+                pstmt.setString(3, user.getPassword());
+                return pstmt;
+            }
+        });
+
+        /*Connection c = null;
+        PreparedStatement pstmt = null;
         try{
             c = connectionMaker.makeConnection();
-            ps = c.prepareStatement(
+            pstmt = c.prepareStatement(
                     "insert into users(id, name, password) values (?,?,?)");
-            ps.setString(1, user.getId());
-            ps.setString(2, user.getName());
-            ps.setString(3, user.getPassword());
+            pstmt.setString(1, user.getId());
+            pstmt.setString(2, user.getName());
+            pstmt.setString(3, user.getPassword());
 
-            ps.executeUpdate();
+            pstmt.executeUpdate();
         } catch (SQLException e){
             throw e;
         }
         finally {
-            if (ps != null) {
+            if (pstmt != null) {
                 try {
-                    ps.close();
+                    pstmt.close();
                 } catch (SQLException e) {
                 }
             }
@@ -47,25 +67,35 @@ public class UserDao {
                 } catch (SQLException e) {
                 }
             }
-        }
+        }*/
     }
 
     public void deleteAll() throws ClassNotFoundException, SQLException{
 
-        Connection c = null;
-        PreparedStatement ps = null;
+        jdbcContextWithStatementStrategy(new StatementStrategy() {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement pstmt = null;
+                pstmt = connection.prepareStatement(
+                        "delete from users");
+                return pstmt;
+            }
+        });
+
+        /*Connection c = null;
+        PreparedStatement pstmt = null;
 
         try {
             c = connectionMaker.makeConnection();
-            ps = c.prepareStatement(
+            pstmt = c.prepareStatement(
                     "delete from users");
-            ps.executeUpdate();
+            pstmt.executeUpdate();
         } catch (SQLException e) {
             throw e;
         } finally {
-            if (ps != null) {
+            if (pstmt != null) {
                 try {
-                    ps.close();
+                    pstmt.close();
                 } catch (SQLException e) {
                 }
             }
@@ -75,19 +105,19 @@ public class UserDao {
                 } catch (SQLException e) {
                 }
             }
-        }
+        }*/
     }
 
     public int getCount() throws ClassNotFoundException,SQLException{
         Connection c = null;
-        PreparedStatement ps = null;
+        PreparedStatement pstmt = null;
         ResultSet rs = null;
 
         try {
             c = connectionMaker.makeConnection();
-            ps = c.prepareStatement(
+            pstmt = c.prepareStatement(
                     "select count(*) from users");
-            rs = ps.executeQuery();
+            rs = pstmt.executeQuery();
             rs.next();
             int count = rs.getInt(1);
             return count;
@@ -99,9 +129,9 @@ public class UserDao {
                     rs.close();
                 }catch (SQLException e){}
             }
-            if(ps!=null){
+            if(pstmt!=null){
                 try{
-                    ps.close();
+                    pstmt.close();
                 }catch (SQLException e){}
             }
             if(c!=null){
@@ -116,19 +146,19 @@ public class UserDao {
     public User findById(String id) throws ClassNotFoundException, SQLException{
         Connection c = connectionMaker.makeConnection();
 
-        PreparedStatement ps = c.prepareStatement(
+        PreparedStatement pstmt = c.prepareStatement(
                 "select * from users where id = ?"
         );
-        ps.setString(1,id);
+        pstmt.setString(1,id);
 
-        ResultSet rs = ps.executeQuery();
+        ResultSet rs = pstmt.executeQuery();
         User user = null;
         if(rs.next()){
             user = new User(rs.getString("id"), rs.getString("name"),
                     rs.getString("password"));
         }
         rs.close();
-        ps.close();
+        pstmt.close();
         c.close();
         if(user == null) throw new EmptyResultDataAccessException(1);
         return user;
