c8cd1df0706c8ba585ddbe8ce26eb7c286f035c7
yejinstar <gyj5586@gmail.com>
2022-10-25 11:10:34 +0900

    Interface 적용해서 ConnectionMaker 분리

---
+++ src/main/java/com/likelion/dao/AwsConnectionMaker.java
@@ -0,0 +1,16 @@
+package com.likelion.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
+
+public class AwsConnectionMaker implements ConnectionMaker{
+    @Override
+    public Connection makeConnection() throws ClassNotFoundException, SQLException {
+        Map<String, String> env = System.getenv();
+        Connection c = DriverManager.getConnection(env.get("DB_HOST"),
+                env.get("DB_USER"), env.get("DB_PASSWORD"));
+        return c;
+    }
+}
