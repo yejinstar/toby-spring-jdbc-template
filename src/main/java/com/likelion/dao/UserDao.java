5a0bd614ff30f63573c8f25aaebf8d42e509f16b
yejinstar <gyj5586@gmail.com>
2022-10-25 10:42:12 +0900

    first commit

---
+++ src/main/java/com/likelion/dao/UserDao.java
@@ -0,0 +1,4 @@
+package com.likelion.dao;
+
+public class UserDao {
+}
---
+++ src/main/java/com/likelion/domain/User.java
@@ -0,0 +1,14 @@
+package com.likelion.domain;
+
+public class User {
+    private String id;
+    private String name;
+    private String password;
+
+    public User(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+}
