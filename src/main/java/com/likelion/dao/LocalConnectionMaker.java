2d4d0e089564def6917499f21650a5dffcd8df08
yejinstar <gyj5586@gmail.com>
2022-10-25 11:25:01 +0900

    Mysql jdbc 드라이버 추가

--- src/main/java/com/likelion/dao/LocalConnectionMaker.java
+++ src/main/java/com/likelion/dao/LocalConnectionMaker.java
@@ -8,6 +8,7 @@ import java.util.Map;
 public class LocalConnectionMaker implements ConnectionMaker{
     @Override
     public Connection makeConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
         Map<String, String> env = System.getenv();
         Connection c = DriverManager.getConnection(env.get("DB_HOST"),
                 env.get("DB_USER"), env.get("DB_PASSWORD"));
